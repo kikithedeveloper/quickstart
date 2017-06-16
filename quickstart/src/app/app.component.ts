6fa4000f639219f3ae9427ecdb2c05cef96e6ebb
590985 <kimberlylin@C02TH0TDHF1T.local>
2017-06-15 23:16:54 -0400

    completed two way data binding

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-
+ 
 export class Hero {
   id: number;
   name: string;
 }
-
+ 
 @Component({
   selector: 'my-app',
   template: `
-    <h2>{{hero.name}}</h2>
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
     </div>
-  `
+    `
 })
-
-export class AppComponent  { 
+export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
